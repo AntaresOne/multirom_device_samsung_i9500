d13d7543887acae9e87e6fc7aff63e0280b39500
AntaresOne <tesla.madal93@gmail.com>
2014-10-03 19:54:07 +0200

    Add MultiROM hooks & init devices

---
+++ multirom/mr_hooks.c
@@ -0,0 +1,12 @@
+// Currently useless. Under development.
+
+/*
+* This file contains device specific hooks.
+* Always enclose hooks to #if MR_DEVICE_HOOKS >= ver
+* with corresponding hook version!
+*/
+#include <stdio.h>
+#include <unistd.h>
+#if MR_DEVICE_HOOKS >= #To be added#
+
+#endif /* MR_DEVICE_HOOKS >= #To be added# */
---
+++ multirom/mr_init_devices.c
@@ -0,0 +1,12 @@
+// Currently useless. Under development.
+
+#include <stdlib.h>
+// These are paths to folders in /sys which contain "uevent" file
+// need to init this device.
+// MultiROM needs to init framebuffer, mmc blocks, input devices,
+// some ADB-related stuff and USB drives, if OTG is supported
+// You can use * at the end to init this folder and all its subfolders
+const char *mr_init_devices[] =
+{
+	 NULL
+};
